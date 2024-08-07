7ca6c270df029538afe2529da0936d4f036adf64
mooojn <moontariq7867@gmail.com>
2024-08-07 15:55:37 +0500

    Add new model

--- task_lyft/__manifest__.py
+++ task_lyft/__manifest__.py
@@ -23,11 +23,11 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/tasklyft_homepage.xml',
-        'views/tasklyft_servicepage.xml',
-        'views/tasklyft_gigpage.xml',
-        'views/tasklyft_nav.xml',
-        'views/tasklyft_footer.xml',
+        # 'views/tasklyft_homepage.xml',
+        # 'views/tasklyft_servicepage.xml',
+        # 'views/tasklyft_gigpage.xml',
+        # 'views/tasklyft_nav.xml',
+        # 'views/tasklyft_footer.xml',
         'views/tasklyft_signup.xml',
         'views/tasklyft_menuitems.xml',
     ],
--- task_lyft/models/tasklyft_service.py
+++ task_lyft/models/tasklyft_service.py
@@ -5,22 +5,33 @@ class tasklyft_service(models.Model):
     _description = 'TaskLyft Service'
 
     title = fields.Char(string='Title', required=True)
-    category = fields.Selection([
-        ('Software Developer', 'Software Developer'), 
-        ('Web Developer', 'Web Developer'), 
-        ('Mobile App Developer', 'Mobile App Developer'), 
-        ('Graphic Designer', 'Graphic Designer'), 
-        ('UI/UX Designer', 'UI/UX Designer'), 
-        ('Data Analyst', 'Data Analyst'), 
-        ('Odoo Developer', 'Odoo Developer'), 
-        ('Word-Press Developer', 'Word-Press Developer')
-    ], string='Category', required=True)
+    
+    is_educator = fields.Boolean(string='Do you want to teach educational courses?', default=False)
+
+    education_category = fields.Selection([
+        ('Matric', 'Matric'), 
+        ('FSC(Pre-Engineering)', 'FSC(Pre-Engineering)'), 
+        ('FSC(Pre-Medical)', 'FSC(Pre-Medical)'), 
+        ('ICS', 'ICS'), 
+        ('FA', 'FA'), 
+        ('I.COM', 'I.COM'),
+    ], string='Education Category')
+
+    skill_category = fields.Selection([
+        ('Python', 'Python'), 
+        ('Java', 'Java'), 
+        ('C++', 'C++'), 
+        ('Web Development', 'Web Development'), 
+        ('Data Science', 'Data Science'), 
+        ('Machine Learning', 'Machine Learning'), 
+        ('Database Management', 'Database Management'),
+    ], string='Skill Category')
+    
     experience_level = fields.Selection([
         ('Beginner', 'Beginner'),
         ('Intermediate', 'Intermediate'), 
         ('Professional', 'Professional')
     ], string='Experience Level')
     picture = fields.Image(string='Picture', required=True)    
-    price_per_hour = fields.Float(string='Price Per Hour(PKR)', required=True)
-    
+    monthly_price = fields.Float(string='Price Per Month(PKR)', required=True)
     user_id = fields.Many2one('res.partner', string="User")
